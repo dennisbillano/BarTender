f4e0720b5e4ac132d0b91e76d08c084edaec8442
Dennis <pi@raspberrypi.(none)>
2014-09-04 06:56:32 +0000

    added styles.css

--- index.php
+++ index.php
@@ -5,15 +5,27 @@ include_once("lib/functions.php");
 
 include_once("templates/header.php");
 
-echo "<h1>Drink menu</h1>";
-echo "<table>\n";
+echo "<h1 class=\"mainHeading\">Drink menu</h1>";
+echo "<table align=\"center\">\n";
 
 $recipes = getRecipes();
+
+$i=0;
 foreach($recipes as $recipe){
-  echo "  <tr> \n";
-  echo "    <td>$recipe</td> \n";
-  echo "    <td><a href=\"viewdrink.php?recipe=$recipe\">View Drink</td> \n";
-  echo "  </tr> \n";
+
+  if($i%3==0)
+    echo "  <tr valign=\"middle\"> \n";
+
+  echo "    <td onClick=\"window.location='viewdrink.php?recipe=$recipe';\" align=\"center\" width=\"350\">\n";
+  echo "       <p class=\"name\">$recipe</p> \n";
+  echo "       <p class=\"description\">$recipe->description</p> \n";
+  echo "    </td> \n";
+
+  $i++;
+
+  if($i%2==0)
+    echo "  </tr> \n";
+
 }
 
 echo "</table> \n";
